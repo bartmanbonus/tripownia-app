22f153235dc951962e210c17b0957e36354aef66
Bonus <malgorzata.bartkiewicz@upbonus.pl>
2026-09-12 10:56:28 +0200

    fix(mobile): use existing www fallback directory

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'pl.tripownia.app',
   appName: 'Tripownia',
-  webDir: 'dist',
+  webDir: 'www',
   server: {
     url: 'https://tripownia.pl/app',
     cleartext: false,
