ef61dcec19b20f1f1434ce8cda71df7f65fa706e
Bonus <malgorzata.bartkiewicz@upbonus.pl>
2026-09-12 21:59:47 +0200

    Wire seasonal SEO landings into travel hub

--- lib/allSeoLandings.ts
+++ lib/allSeoLandings.ts
@@ -1,8 +1,9 @@
 import { seoLandings as baseSeoLandings } from "@/lib/seoLandings";
 import { seoGrowthLandings } from "@/lib/seoGrowthLandings";
 import { seoAirportLandings } from "@/lib/seoAirportLandings";
+import { seoSeasonalLandings } from "@/lib/seoSeasonalLandings";
 
-export const allSeoLandings = [...baseSeoLandings, ...seoGrowthLandings, ...seoAirportLandings];
+export const allSeoLandings = [...baseSeoLandings, ...seoGrowthLandings, ...seoAirportLandings, ...seoSeasonalLandings];
 
 export function getAllSeoLanding(slug: string) {
   return allSeoLandings.find((item) => item.slug === slug);
