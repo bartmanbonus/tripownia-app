74bd1f2d88a5fb7ad6046ea0c06c9ccc7655d8ff
Bonus <malgorzata.bartkiewicz@upbonus.pl>
2026-09-26 19:51:42 +0200

    Default destination search to holidays

--- components/DestinationLandingPanel.tsx
+++ components/DestinationLandingPanel.tsx
@@ -163,7 +163,7 @@ export default function DestinationLandingPanel({ path }: { path: string }) {
             <p>Kierunek jest już uzupełniony. Zmień tylko to, co ma znaczenie dla Twojego wyjazdu.</p>
           </div>
         </div>
-        <SearchHub embedded initialDestinations={[config.searchDestination]} destinationQuickPicks={config.popular} />
+        <SearchHub embedded initialTab="Wakacje" initialDestinations={[config.searchDestination]} destinationQuickPicks={config.popular} />
       </section>
 
       <section className={styles.offersSection}>
